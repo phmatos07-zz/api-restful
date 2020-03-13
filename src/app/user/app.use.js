47c2c726f498c7f2c1a5f56f2fedb5087b057701
PHMATOS07 <phelipe.matos07@gmail.com>
2020-03-13 00:10:59 -0300

    Definição do arquivo responsável por gerenciar as rotas do CRUD dos usuários (User).

--- src/app/user/app.use.js
+++
@@ -1,9 +0,0 @@
-const app = require('../../app.js');
-const createUser = require('./create-user/create-user.route.js');
-
-// POST
-app.set('./create-user', createUser);
-
-// GET
-// PUT
-// DELETE
