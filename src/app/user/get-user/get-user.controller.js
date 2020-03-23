adecccb6ba403efb42d88c0043e4251ed13335fa
Phelipe Matos <phelipe.matos-ext@viavarejo.com.br>
2020-03-23 17:24:19 -0300

    Teste de utilização do middleware 'clientErrorHandlerMiddleware'

--- src/app/user/get-user/get-user.controller.js
+++ src/app/user/get-user/get-user.controller.js
@@ -1,5 +1,16 @@
-const getUser = (req, res) => {
-  return res.send('Teste de GET');
+const createObjectError = require('./../../../helpers/create-object-error/create-object-error.helper');
+const typeErrorsEnum = require('./../../../models/enum/type-errors.enum');
+
+const getUser = (req, res, next) => {
+
+  try {
+    // Em desenvolvimento
+    res.send('Teste');
+    throw createObjectError(typeErrorsEnum.XML_HTTP_REQUEST, 200, 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.');
+
+  } catch (error) {
+    return next(error);
+  }
 };
 
 module.exports = getUser;
