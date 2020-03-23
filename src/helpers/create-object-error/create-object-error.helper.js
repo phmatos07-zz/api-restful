ba70a909daf0ea4c32c66325e70a8fbc9ddff550
Phelipe Matos <phelipe.matos-ext@viavarejo.com.br>
2020-03-23 17:21:24 -0300

    Criação da função 'createObjectError'

---
+++ src/helpers/create-object-error/create-object-error.helper.js
@@ -0,0 +1,10 @@
+const createObjectError = (typeErrors, statusCode, message) => {
+
+  const error = new Error();
+  error.typeError = typeErrors;
+  error.httpStatusCode = statusCode;
+  error.message = message;
+  return error;
+};
+
+module.exports = createObjectError;
