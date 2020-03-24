577a87ed71d2f392ad3ba366c9ba7216b72a98dd
Phelipe Matos <phelipe.matos-ext@viavarejo.com.br>
2020-03-24 12:22:42 -0300

    Criação e implmentação do ENUM 'ResponsesStatusEnum'

--- src/app/user/get-user/get-user.controller.js
+++ src/app/user/get-user/get-user.controller.js
@@ -1,12 +1,17 @@
 const createObjectError = require('./../../../helpers/create-object-error/create-object-error.helper');
 const typeErrorsEnum = require('./../../../models/enum/type-errors.enum');
+const tesponsesStatusEnum = require('./../../../models/enum/responses-status.enum');
 
 const getUser = (req, res, next) => {
 
   try {
     // Em desenvolvimento
-    res.send('Teste');
-    throw createObjectError(typeErrorsEnum.XML_HTTP_REQUEST, 200, 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.');
+    // res.send('Teste');
+    throw createObjectError(
+      typeErrorsEnum.XML_HTTP_REQUEST,
+      tesponsesStatusEnum.NOT_FOUND,
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+    );
 
   } catch (error) {
     return next(error);
